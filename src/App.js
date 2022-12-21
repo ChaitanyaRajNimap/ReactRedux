a175b7ffba380f19b0846387836bb622b7cca81c
ChaitanyaRajNimap <chaitanyarajiwade@nimapinfotech.com>
2022-12-21 19:00:52 +0530

    store added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import CakeContainer from "./components/CakeContainer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Redux</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>React Redux</h1>
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
