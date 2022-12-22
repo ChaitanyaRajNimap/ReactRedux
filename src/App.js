3f84c8a9285352038df40981b1f0f6e3516e8f1d
ChaitanyaRajNimap <chaitanyarajiwade@nimapinfotech.com>
2022-12-22 18:52:52 +0530

    Action payload implememnted

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
+import IceCreamContainer from "./components/IceCreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <h1>React Redux</h1>
+        <HooksCakeContainer />
         <CakeContainer />
+        <IceCreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
